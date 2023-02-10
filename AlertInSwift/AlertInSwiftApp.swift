aa3db078b6267b140d58870f63c7cd3dee9e5f62
Bravestone <gauravmalimj@gmail.com>
2023-02-10 09:48:35 +0530

    Initial Commit

---
+++ AlertInSwift/AlertInSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlertInSwiftApp.swift
+//  AlertInSwift
+//
+//  Created by gaurav mali on 10/02/23.
+//
+
+import SwiftUI
+
+@main
+struct AlertInSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
